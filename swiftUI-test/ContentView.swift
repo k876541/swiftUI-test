e7ae34c8ac6edbc5c9d13a5683ab089de09aff6b
ryan_chang <k876541@hotmail.com>
2021-03-13 01:25:24 +0800

    Initial Commit

---
+++ swiftUI-test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUI-test
+//
+//  Created by Ryan Chang on 2021/3/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
